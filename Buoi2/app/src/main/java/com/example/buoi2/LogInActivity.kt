32d9b342449d4ce45e6757607d061e9bbad888dd
NguyenMinh205 <nqm1312@gmail.com>
2026-03-24 22:15:24 +0700

    chore: Add logic change Activity in Buoi2

--- Buoi2/app/src/main/java/com/example/buoi2/LogInActivity.kt
+++ Buoi2/app/src/main/java/com/example/buoi2/LogInActivity.kt
@@ -1,6 +1,8 @@
 package com.example.buoi2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,6 +26,28 @@ class LogInActivity : AppCompatActivity() {
             insets
         }
 
-        
+        binding.tvSignUp.setOnClickListener {
+            var intent = Intent(this, SignUpActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        binding.logInBtn.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập đầy đủ Email và Mật khẩu!", Toast.LENGTH_SHORT).show()
+            }
+            else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "Vui lòng nhập đúng định dạng Email!", Toast.LENGTH_SHORT).show()
+            }
+            else if (password.length < 8) {
+                Toast.makeText(this, "Mật khẩu phải dài ít nhất 8 ký tự!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- Buoi2/app/src/main/java/com/example/buoi2/SignUpActivity.kt
+++ Buoi2/app/src/main/java/com/example/buoi2/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.example.buoi2
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +23,41 @@ class SignUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.tvLogIn.setOnClickListener {
+            finish()
+        }
+
+        binding.ivBack.setOnClickListener {
+             finish()
+        }
+
+        binding.registerBtn.setOnClickListener {
+            val fullName = binding.etFullName.text.toString().trim()
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (fullName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.etEmail.error = "Email sai định dạng (ví dụ: abc@gmail.com)"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 8) {
+                binding.etPassword.error = "Mật khẩu phải dài ít nhất 8 ký tự!"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this, "Dữ liệu hợp lệ! Đang xử lý đăng ký...", Toast.LENGTH_SHORT).show()
+
+            finish()
         }
     }
 }
